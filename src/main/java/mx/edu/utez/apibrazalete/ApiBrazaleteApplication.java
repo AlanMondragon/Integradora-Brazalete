fa2935b54ecb9e85069201d5620eeeb85ae5b03a
El-Actor-Alan-Versions <20233tn075@utez.edu.mx>
2025-02-10 18:52:27 -0600

    Creacion del proyecto

---
+++ src/main/java/mx/edu/utez/apibrazalete/ApiBrazaleteApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.apibrazalete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBrazaleteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBrazaleteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/apibrazalete/ApiBrazaleteApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.apibrazalete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBrazaleteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
